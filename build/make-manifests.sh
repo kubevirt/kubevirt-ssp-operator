c8f4cc78c3c3ae89079e67003f50f3ecca243af5
Francesco Romani <fromani@redhat.com>
2019-04-16 17:06:26 +0200

    manifests: add all-in-one manifests
    - generated from master sources under deploy/ - so no duplication
    - to ease the integration with HCO
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ build/make-manifests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# generate all-in-one manifests, easier to consume, e.g. for HCO integration
+# the master source the content of deploy/
+
+VERSION="${1:-0.0.0}"
+MANIFESTS_DIR="manifests/v${VERSION}"
+
+mkdir -p ${MANIFESTS_DIR}
+
+(
+for CRD in $( ls deploy/crds/kubevirt_*crd.yaml ); do
+	echo "---"
+	cat ${CRD}
+done
+) > ${MANIFESTS_DIR}/kubevirt-ssp-operator-v${VERSION}.crds.yaml
+
+(
+for CR in $( ls deploy/crds/kubevirt_*cr.yaml ); do
+	echo "---"
+	cat ${CR}
+done
+) > ${MANIFESTS_DIR}/kubevirt-ssp-operator-v${VERSION}.crs.yaml
+
+(
+for MF in deploy/service_account.yaml deploy/role.yaml deploy/role_binding.yaml deploy/operator.yaml; do
+	echo "---"
+	cat ${MF}
+done
+) > ${MANIFESTS_DIR}/kubevirt-ssp-operator-v${VERSION}.yaml
+
+echo "built these manifests:"
+ls ${MANIFESTS_DIR}/*.yaml
