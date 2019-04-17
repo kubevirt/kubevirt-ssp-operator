fb0e300bea10eab82d0c67311b89f016320ce378
Francesco Romani <fromani@redhat.com>
2019-04-17 13:14:09 +0200

    manifests: overhaul the layout
    This patch fixes the manifests layout to:
    1. provide a tree layout comformant to https://github.com/operator-framework/operator-registry#manifest-format
       and easy to consume for registry images
    2. split the cluster all-in-one manifests (used to make integration with
       HCO easier) into a separate subtree.
    
    The final layout is:
    deploy/    - master source of truth, everything else is created from the
                 manifests found here
    manifests/ - operator-framework compatible layout
    cluster/   - amalgamation of manifests found in deploy/, easier to
                 consume - but no new content

--- build/make-manifests.sh
+++ build/make-manifests.sh
@@ -1,34 +1,68 @@
 #!/bin/bash
 
-# generate all-in-one manifests, easier to consume, e.g. for HCO integration
-# the master source the content of deploy/
-
 VERSION="${1:-0.0.0}"
-MANIFESTS_DIR="manifests/v${VERSION}"
+CLUSTER_VERSIONED_DIR="cluster/${VERSION}"
+MANIFESTS_DIR="manifests/kubevirt-ssp-operator"
+MANIFESTS_VERSIONED_DIR="${MANIFESTS_DIR}/v${VERSION}"
 IMAGE_PATH="quay.io/fromani/kubevirt-ssp-operator-container:latest"
 
-mkdir -p ${MANIFESTS_DIR}
+# TODO: tested with operator-sdk 0.7.0: should we require it?
+which operator-sdk &> /dev/null || {
+	echo "operator-sdk not found (see https://github.com/operator-framework/operator-sdk)"
+	exit 1
+}
+
+which operator-courier &> /dev/null || {
+	echo "operator-courier not found (see https://github.com/operator-framework/operator-courier)"
+	exit 2
+}
+
+
+mkdir -p ${CLUSTER_VERSIONED_DIR}
+mkdir -p ${MANIFESTS_VERSIONED_DIR}
 
 (
 for CRD in $( ls deploy/crds/kubevirt_*crd.yaml ); do
 	echo "---"
 	cat ${CRD}
 done
-) > ${MANIFESTS_DIR}/kubevirt-ssp-operator-v${VERSION}.crds.yaml
+) > ${CLUSTER_VERSIONED_DIR}/kubevirt-ssp-operator-crd.yaml
 
 (
 for CR in $( ls deploy/crds/kubevirt_*cr.yaml ); do
 	echo "---"
 	cat ${CR}
 done
-) > ${MANIFESTS_DIR}/kubevirt-ssp-operator-v${VERSION}.crs.yaml
+) > ${CLUSTER_VERSIONED_DIR}/kubevirt-ssp-operator-cr.yaml
 
 (
 for MF in deploy/service_account.yaml deploy/role.yaml deploy/role_binding.yaml deploy/operator.yaml; do
 	echo "---"
 	sed "s|REPLACE_IMAGE|${IMAGE_PATH}|" < ${MF}
 done
-) > ${MANIFESTS_DIR}/kubevirt-ssp-operator-v${VERSION}.yaml
+) > ${CLUSTER_VERSIONED_DIR}/kubevirt-ssp-operator.yaml
 
-echo "built these manifests:"
-ls ${MANIFESTS_DIR}/*.yaml
+operator-sdk olm-catalog gen-csv --csv-version ${VERSION}
+
+./build/update-olm.py \
+	deploy/olm-catalog/kubevirt-ssp-operator/${VERSION}/kubevirt-ssp-operator.v${VERSION}.clusterserviceversion.yaml > \
+	${MANIFESTS_VERSIONED_DIR}/kubevirt-ssp-operator.v${VERSION}.clusterserviceversion.yaml
+
+# caution: operator-courier (as in 5a4852c) whants *one* entity per yaml file (e.g. it does NOT use safe_load_all)
+for CRD in $( ls deploy/crds/kubevirt_*crd.yaml ); do
+	cp ${CRD} ${MANIFESTS_VERSIONED_DIR}
+done
+
+cat << EOF > ${MANIFESTS_VERSIONED_DIR}/kubevirt-ssp-operator.package.yaml
+packageName: kubevirt-ssp-operator
+channels:
+- name: beta
+  currentCSV: kubevirt-ssp-operator.v${VERSION}
+EOF
+
+## needed to build the registry
+cp ${MANIFESTS_VERSIONED_DIR}/kubevirt-ssp-operator.package.yaml ${MANIFESTS_DIR}
+
+operator-courier verify ${MANIFESTS_VERSIONED_DIR} && echo "OLM verify passed" || echo "OLM verify failed"
+
+rm ${MANIFESTS_VERSIONED_DIR}/kubevirt-ssp-operator.package.yaml
