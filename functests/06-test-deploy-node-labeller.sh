d2a6dc231157c332e48dece8efd2f25cfc6bf5e2
Francesco Romani <fromani@redhat.com>
2019-05-22 14:02:15 +0200

    functests: add basic node labeller test
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ functests/06-test-deploy-node-labeller.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SCRIPTPATH=$( dirname $(readlink -f $0) )
+source ${SCRIPTPATH}/testlib.sh
+
+[ -z ${KV_NAMESPACE} ] && exit 127
+
+RET=1
+TEST_NS="${KV_NAMESPACE}"
+
+oc create -n ${TEST_NS} -f "${SCRIPTPATH}/node-labeller-unversioned-cr.yaml" || exit 2
+# TODO: SSP-operator needs to improve its feedback mechanism
+sleep 10s
+for idx in $( seq 1 30); do
+	ANNOTATIONS=$( oc get nodes -o json | jq '.items[0].metadata.annotations | keys' )
+	# any random CPU model annotation that *must* be present if everything's OK
+	HAS_CPU=$( echo ${ANNOTATIONS} | jq 'map(endswith("cpu-model-kvm64")) | any' )
+	# any random KVM info annotation that *must* be present if everything's OK
+	HAS_KVM=$( echo ${ANNOTATIONS} | jq 'map(endswith("kvm-info-cap-hyperv-base")) | any' )
+	if [ "${HAS_CPU}" == "true" ] && [ "${HAS_KVM}" == "true" ]; then
+		RET=0
+		break
+	fi
+	sleep 2s
+done
+oc delete -n ${TEST_NS} -f "${SCRIPTPATH}/node-labeller-unversioned-cr.yaml" || exit 2
+
+exit $RET
--- functests/testlib.sh
+++ functests/testlib.sh
@@ -2,11 +2,21 @@
 
 export SSP_OP_POD_NAME=""
 export SSP_OP_POD_NAMESPACE=""
-export SSP_OP_MANIFEST_JSON=$(oc get pods --all-namespaces -l name=kubevirt-ssp-operator -o json | jq .items[0] )
+export SSP_OP_MANIFEST_JSON=$( oc get pods --all-namespaces -l name=kubevirt-ssp-operator -o json | jq .items[0] )
 
 if [ -z "${SSP_OP_MANIFEST_JSON}" ] || [ "${SSP_OP_MANIFEST_JSON}" == "null" ]; then
 	export SSP_OP_MANIFEST_JSON=""
 else
 	export SSP_OP_POD_NAME=$( echo "${SSP_OP_MANIFEST_JSON}" | jq -r .metadata.name )
 	export SSP_OP_POD_NAMESPACE=$( echo "${SSP_OP_MANIFEST_JSON}" | jq -r .metadata.namespace )
+fi
+
+export KV_NAMESPACE=""
+export KV_OP_POD_NAMESPACE=""
+export KV_OP_MANIFEST_JSON=$( oc get pods --all-namespaces -l 'kubevirt.io=virt-operator' -o json | jq -r '.items[0]' )
+if [ -z "${KV_OP_MANIFEST_JSON}" ] || [ "${KV_OP_MANIFEST_JSON}" == "null" ]; then
+	export KV_OP_MANIFEST_JSON=""
+else
+	export KV_OP_POD_NAMESPACE=$( echo "${KV_OP_MANIFEST_JSON}" | jq -r .metadata.namespace )
+	export KV_NAMESPACE="${KV_OP_POD_NAMESPACE}"
 fi
