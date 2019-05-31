62562a6b87b8f87ba307459d406e67b3adbd0ad6
Francesco Romani <fromani@redhat.com>
2019-06-01 01:15:14 +0200

    functests: more debug logs
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- functests/04-test-deploy-templates-operator-namespace.sh
+++ functests/04-test-deploy-templates-operator-namespace.sh
@@ -13,7 +13,9 @@ oc create -n ${TEST_NS} -f "${SCRIPTPATH}/common-templates-versioned-cr.yaml" ||
 sleep 10s
 for idx in $( seq 1 30); do
 	NUM=$(oc get templates -n ${TEST_NS} -l "template.kubevirt.io/type=base" -o json | jq ".items | length")
+	(( ${V} >= 1 )) && echo "templates found in ${TEST_NS}: ${NUM}"
 	if (( "${NUM}" > 0 )); then
+		(( ${V} >= 1 )) && echo "enough templates found in ${TEST_NS}"
 		RET=0
 		break
 	fi
