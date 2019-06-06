2fb0a75a69e6427714d2482b0c690920334a805b
Francesco Romani <fromani@redhat.com>
2019-06-06 16:33:16 +0200

    docs: install reuse the travis script
    With minor changes, we can provide a simple install
    script for everyone brave enough to install the
    SSP-operator in their cluster.
    
    This simplifies the install process quite a lot
    with no real drawbacks.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack/install-operator.sh
+++ hack/install-operator.sh
@@ -10,20 +10,32 @@ oc create -f ${BASEPATH}/../deploy/crds/kubevirt_v1_commontemplatesbundle_crd.ya
 oc create -f ${BASEPATH}/../deploy/crds/kubevirt_v1_nodelabellerbundle_crd.yaml
 oc create -f ${BASEPATH}/../deploy/crds/kubevirt_v1_templatevalidator_crd.yaml
 
+LAST_TAG=""
+if [ "${CI}" != "true" ] || [ "${TRAVIS}" != "true" ]; then
+	# TODO: consume releases, not tags
+	LAST_TAG=$( curl -s https://api.github.com/repos/MarSik/kubevirt-ssp-operator/tags | jq -r '.[0].name' )
+fi
+
 oc create -n ${NAMESPACE} -f ${BASEPATH}/../deploy/service_account.yaml
 oc create -n ${NAMESPACE} -f ${BASEPATH}/../deploy/role.yaml
 
 sed "s|namespace: kubevirt|namespace: ${NAMESPACE}|g" < ${BASEPATH}/../deploy/role_binding.yaml | \
 	oc create -n ${NAMESPACE} -f -
 
-REGISTRY=$(minishift openshift registry)
-sed "s|REPLACE_IMAGE|${REGISTRY}/kubevirt/kubevirt-ssp-operator:devel|g" < ${BASEPATH}/../deploy/operator.yaml | \
-	sed "s|imagePullPolicy: Always|imagePullPolicy: IfNotPresent|g" | \
-	oc create -n ${NAMESPACE} -f -
+if [ "${CI}" == "true" ] && [ "${TRAVIS}" == "true" ]; then
+	REGISTRY=$(minishift openshift registry)
+	sed "s|REPLACE_IMAGE|${REGISTRY}/kubevirt/kubevirt-ssp-operator:devel|g" < ${BASEPATH}/../deploy/operator.yaml | \
+		sed "s|imagePullPolicy: Always|imagePullPolicy: IfNotPresent|g" | \
+		oc create -n ${NAMESPACE} -f -
 
-sleep 5s
+	sleep 5s
 
-oc get pods -n ${NAMESPACE}
-oc get pod -n ${NAMESPACE} -l "name=kubevirt-ssp-operator" -o yaml
+	oc get pods -n ${NAMESPACE}
+	oc get pod -n ${NAMESPACE} -l "name=kubevirt-ssp-operator" -o yaml
 
-sleep 5s
+	sleep 5s
+else
+	sed "s|REPLACE_IMAGE|quay.io/fromani/kubevirt-ssp-operator-container:${LAST_TAG}|g" < ${BASEPATH}/../deploy/operator.yaml | \
+		grep -v FIXME | \
+		oc create -n ${NAMESPACE} -f -
+fi
