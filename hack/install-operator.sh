807d63a75e853bf05ef37ea66caae4e4e3a9a79f
Francesco Romani <fromani@redhat.com>
2019-06-11 17:55:57 +0200

    manifests: install: fix RBAC permissions
    The PR https://github.com/MarSik/kubevirt-ssp-operator/pull/27
     had a bug which was not evident on standalone installation, because in
    the install script we granted too broad permissions.
    
    This became evident when deployed under HCO.
    This PR aims to fix the RBAC permission issues without using too broad
    permission sets.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack/install-operator.sh
+++ hack/install-operator.sh
@@ -6,17 +6,17 @@ BASEPATH=$( dirname $SELF )
 
 NAMESPACE=${1:-kubevirt}
 
-REGISTRY=$(minishift openshift registry)
-
-oc create -n ${NAMESPACE} -f ${BASEPATH}/../deploy/service_account.yaml
-oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:${NAMESPACE}:kubevirt-ssp-operator
-oc create -n ${NAMESPACE} -f ${BASEPATH}/../deploy/role.yaml
-oc create -n ${NAMESPACE} -f ${BASEPATH}/../deploy/role_binding.yaml
-
 oc create -f ${BASEPATH}/../deploy/crds/kubevirt_v1_commontemplatesbundle_crd.yaml
 oc create -f ${BASEPATH}/../deploy/crds/kubevirt_v1_nodelabellerbundle_crd.yaml
 oc create -f ${BASEPATH}/../deploy/crds/kubevirt_v1_templatevalidator_crd.yaml
 
+oc create -n ${NAMESPACE} -f ${BASEPATH}/../deploy/service_account.yaml
+oc create -n ${NAMESPACE} -f ${BASEPATH}/../deploy/role.yaml
+
+sed "s|namespace: kubevirt|namespace: ${NAMESPACE}|g" < ${BASEPATH}/../deploy/role_binding.yaml | \
+	oc create -n ${NAMESPACE} -f -
+
+REGISTRY=$(minishift openshift registry)
 sed "s|REPLACE_IMAGE|${REGISTRY}/kubevirt/kubevirt-ssp-operator:devel|g" < ${BASEPATH}/../deploy/operator.yaml | \
 	sed "s|imagePullPolicy: Always|imagePullPolicy: IfNotPresent|g" | \
 	oc create -n ${NAMESPACE} -f -
