4234a6d75921338e5b13f892a638bb4b535c3906
Karel Šimon <ksimon@redhat.com>
2019-08-14 10:16:54 +0200

    fixed wrong return codes

--- functests/08-test-conditions-node-labeller.sh
+++ functests/08-test-conditions-node-labeller.sh
@@ -8,26 +8,25 @@ source ${SCRIPTPATH}/testlib.sh
 RET=0
 TEST_NS="${KV_NAMESPACE}"
 
-exit 0
-
 oc create -n ${TEST_NS} -f "${SCRIPTPATH}/node-labeller-unversioned-cr.yaml" || exit 2
 # TODO: SSP-operator needs to improve its feedback mechanism
 # fetching node-labeller images may take a while
 
 wait_node_labeller_running ${TEST_NS} 5 60
+
 #wait for ssp operator to set proper conditions
 sleep 15s
 
-if [ $(oc get KubevirtNodeLabellerBundle -o json | jq  '.items[0].status.conditions[] | select((.type=="Progressing") and (.status="True")) | .type' | wc -l) -eq 0 ]; then
-		RET=0
-fi
-
 if [ $(oc get KubevirtNodeLabellerBundle -o json | jq  '.items[0].status.conditions[] | select((.type=="Progressing") and (.status="False")) | .type' | wc -l) -eq 0 ]; then
-		RET=0
+	echo "Progressing condition is not set to false"
+	RET=1
+	exit $RET
 fi
 
 if [ $(oc get KubevirtNodeLabellerBundle -o json | jq  '.items[0].status.conditions[] | select((.type=="Available") and (.status="True")) | .type' | wc -l) -eq 0 ]; then
-		RET=0
+	echo "Available condition is not set to true"
+	RET=1
+	exit $RET
 fi
 
 oc delete -n ${TEST_NS} -f "${SCRIPTPATH}/node-labeller-unversioned-cr.yaml" || exit 2
