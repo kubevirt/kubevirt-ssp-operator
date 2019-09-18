cdeb1a95d006e92f76334a03a95b40cd1b7063d0
Martin Sivák <msivak@redhat.com>
2019-09-18 19:27:53 +0200

    Increase template install timeout in functests

--- functests/04-test-deploy-templates-operator-namespace.sh
+++ functests/04-test-deploy-templates-operator-namespace.sh
@@ -11,7 +11,7 @@ TEST_NS="${SSP_OP_POD_NAMESPACE}"
 oc create -n ${TEST_NS} -f "${SCRIPTPATH}/common-templates-versioned-cr.yaml" || exit 2
 # TODO: SSP-operator needs to improve its feedback mechanism
 sleep 21s
-for idx in $( seq 1 30); do
+for idx in $( seq 1 40); do
 	NUM=$(oc get templates -n ${TEST_NS} -l "template.kubevirt.io/type=base" -o json | jq ".items | length")
 	(( ${V} >= 1 )) && echo "templates found in ${TEST_NS}: ${NUM}"
 	if (( "${NUM}" > 0 )); then
