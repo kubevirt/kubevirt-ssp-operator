ac2fe925d5023ece2b73ecda572d06c908e7beed
Karel Šimon <ksimon@redhat.com>
2020-06-10 14:27:44 +0200

    Merge pull request #181 from ksimon1/fix-labeller-no-kvm
    fix labeller when no kvm device is available

--- functests/06-test-deploy-node-labeller.sh
+++ functests/06-test-deploy-node-labeller.sh
@@ -14,26 +14,6 @@ oc apply -n ${TEST_NS} -f "${SCRIPTPATH}/node-labeller-unversioned-cr.yaml" || e
 
 wait_node_labeller_running ${TEST_NS} 5 60
 
-for idx in $( seq 1 30); do
-	echo "Waiting for node-labeller to label nodes"
-
-	number_of_cpu=$( oc get nodes -o json | jq '.items[0].metadata.labels | keys | map(select(startswith("feature.node.kubernetes.io/cpu-model-"))) | length')
-	number_of_cpu_features=$( oc get nodes -o json | jq '.items[0].metadata.labels | keys | map(select(startswith("feature.node.kubernetes.io/cpu-feature-"))) | length')
-
-	echo "Number of CPU labels: $number_of_cpu"
-	echo "Number of CPU features: $number_of_cpu_features"
-
-	if [ $number_of_cpu -gt 0 ] && [ $number_of_cpu_features -gt 0 ]; then
-		RET=0
-		break
-	fi
-	sleep 2s
-done
-
-if [ $RET -eq 1 ] ; then
-	exit 2
-fi
-
 #wait for ssp operator to set proper conditions
 wait_for_condition ${TEST_NS} 5 40 "KubevirtNodeLabellerBundle" "Available" "True"
 RET="$?"
