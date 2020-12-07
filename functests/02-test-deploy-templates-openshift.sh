60ba09a763e8cb1809842c3c5915ff3a80315396
Omer Yahud <oyahud@redhat.com>
2020-12-07 15:24:10 +0200

    Previously, all templates were aligned to the same version, but a previous fix (https://github.com/kubevirt/kubevirt-ssp-operator/pull/253) locked deprecated templates to older versions, so the operator did not report the Available condition.
    This fix makes the operator report Available when all templates were deployed, regardless of their version.
    
    Signed-off-by: Omer Yahud <oyahud@redhat.com>

--- functests/02-test-deploy-templates-openshift.sh
+++ functests/02-test-deploy-templates-openshift.sh
@@ -7,18 +7,9 @@ RET=1
 TEST_NS="openshift"  # TODO: check this exists?
 
 oc create -n ${TEST_NS} -f "${SCRIPTPATH}/common-templates-unversioned-cr.yaml" || exit 2
-# TODO: SSP-operator needs to improve its feedback mechanism
-sleep 21s
-for idx in $( seq 1 40); do
-	NUM=$(oc get templates -n ${TEST_NS} -l "template.kubevirt.io/type=base" -o json | jq ".items | length")
-	(( ${V} >= 1 )) && echo "templates found in ${TEST_NS}: ${NUM}"
-	if (( "${NUM}" > 0 )); then
-		(( ${V} >= 1 )) && echo "enough templates found in ${TEST_NS}"
-		RET=0
-		break
-	fi
-	sleep 3s
-done
+
+oc wait -f "${SCRIPTPATH}/common-templates-unversioned-cr.yaml" -n ${TEST_NS} --for=condition=Available --timeout=600s
+
 oc delete -n ${TEST_NS} -f "${SCRIPTPATH}/common-templates-unversioned-cr.yaml" || exit 2
 
 exit $RET
--- functests/03-test-deploy-templates-different-namespace.sh
+++ functests/03-test-deploy-templates-different-namespace.sh
@@ -8,18 +8,9 @@ TEST_NS=$(uuidgen)
 
 oc create ns ${TEST_NS} || exit 2
 oc create -n ${TEST_NS} -f "${SCRIPTPATH}/common-templates-unversioned-cr.yaml" || exit 2
-# TODO: SSP-operator needs to improve its feedback mechanism
-sleep 21s
-for idx in $( seq 1 40); do
-	NUM=$(oc get templates -n ${TEST_NS} -l "template.kubevirt.io/type=base" -o json | jq ".items | length")
-	(( ${V} >= 1 )) && echo "templates found in ${TEST_NS}: ${NUM}"
-	if (( "${NUM}" > 0 )); then
-		(( ${V} >= 1 )) && echo "enough templates found in ${TEST_NS}"
-		RET=0
-		break
-	fi
-	sleep 3s
-done
+
+oc wait -f "${SCRIPTPATH}/common-templates-unversioned-cr.yaml" -n ${TEST_NS} --for=condition=Available --timeout=600s
+
 oc delete -n ${TEST_NS} -f "${SCRIPTPATH}/common-templates-unversioned-cr.yaml" || exit 2
 oc delete ns ${TEST_NS} || exit 2
 
